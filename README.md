7c18932489de28f6fcf5d73e44c0e137acf59084
upa-r-upa <me.uparupa@gmail.com>
2024-02-20 17:50:33 +0900

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# taskie_application
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/taskie_application/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.upa-r-upa.taskie_application
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
